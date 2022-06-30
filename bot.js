54bc8ee2a27d2a077d862474bb997814bb9e0792
TechwithAmee1 <107279170+TechwithAmee1@users.noreply.github.com>
2022-06-30 23:02:37 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,15 +33,15 @@ global.APIKeys = {
 }
 
 //settings
-global.owner = ['27634090203',''] //owner number +27634090203
-global.pemilik = ['27634090203'] //owner number +27634090203
-global.premium = ['27634090203'] //premium number +27634090203
+global.owner = ['+18322109598','+17816136668'] //owner number +18322109598
+global.pemilik = ['+18322109598'] //owner number +18322109598
+global.premium = ['+17816136668'] //premium number +18322109598
 global.pengguna = 'Drips' //username
-global.footer = 'ZIM BOT INC'
-global.botnma = 'ZIM BOT INC' //bot name
-global.ownernma = 'Drips' //owner name
-global.packname = 'Drips' //sticker package name
-global.author = 'By Drips Memes' //sticker author name
+global.footer = 'AMIYA ZIM BOT'
+global.botnma = 'AMIYA ZIM BOT' //bot name
+global.ownernma = 'Amiya' //owner name
+global.packname = 'Amiya' //sticker package name
+global.author = 'By Amiya bot Memes' //sticker author name
 global.sessionName = 'session' //session name
 global.prefa = ['#','!','/',''] //prefix
 global.sp = '🔵' //sp
